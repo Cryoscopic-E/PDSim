88bdbd05f5b38ce3de2ff8088626c1146d206d71
Emanuele De Pellegrin <emanueledepellegrin@live.it>
2023-01-16 12:13:47 +0000

    Removed Unused Plugins, Added connection to Backend Server

---
+++ Assets/Scripts/Utils/UnityThread.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using UnityEngine;
+
+namespace PDSim.Utils
+{
+    public abstract class UnityThread
+    {
+        private Thread _thread;
+        protected bool IsRunning { get; private set; }
+
+        protected abstract void Run();
+
+        public void Start()
+        {
+            if (IsRunning)
+                throw new UnityException("Thread started");
+            Debug.Log("Thread started");
+            IsRunning = true;
+            _thread = new Thread(Run);
+            _thread.Start();
+
+        }
+
+        public void Stop()
+        {
+            if (!IsRunning) return;
+            IsRunning = false;
+            Debug.LogWarning("Thread stopped");
+            _thread.Join();
+        }
+    }
+}
