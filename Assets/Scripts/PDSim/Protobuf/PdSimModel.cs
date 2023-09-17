5f8a0ee04c69b371b6dccdabf611e61ffc75be0c
Emanuele <emanueledepellegrin@live.it>
2023-09-17 16:06:32 +0100

    refactor

--- Assets/Scripts/PDSim/Protobuf/PdSimModel.cs
+++ Assets/Scripts/PDSim/Protobuf/PdSimModel.cs
@@ -8,7 +8,6 @@ namespace PDSim.Protobuf
     /// Class representing an atom.
     /// e.g 3, true, kitchen, at-robot
     /// </summary>
-
     public class PdSimAtom
     {
         private Atom.ContentOneofCase _contentCase;
@@ -88,11 +87,13 @@ namespace PDSim.Protobuf
         }
 
     }
+
+
+
     /// <summary>
     /// Class representing a parameter for a fluent, action or function
     /// e.g. ?x - object
     /// </summary>
-
     [Serializable]
     public class PdSimParameter
     {
@@ -202,5 +203,167 @@ namespace PDSim.Protobuf
         }
     }
 
+
+    /// <summary>
+    /// Base class that represents an action in the domain.
+    /// </summary>
+    public abstract class PdSimAction
+    {
+        /// <summary>
+        /// Action name.
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// Action parameters.
+        /// e.g. (?t - truck, ?l1 - location, ?l2 - location)
+        /// </summary>
+        public List<PdSimParameter> parameters;
+
+        /// <summary>
+        /// List of preconditions of the action.
+        /// </summary>
+        public List<PdSimCondition> preconditions;
+        /// <summary>
+        /// List of effects of the action.
+        /// </summary>
+        public List<PdSimEffect> effects;
+        public PdSimAction(Action action)
+        {
+            name = action.Name;
+
+            parameters = new List<PdSimParameter>();
+            foreach (var parameter in action.Parameters)
+            {
+                parameters.Add(new PdSimParameter(parameter));
+            }
+
+            preconditions = new List<PdSimCondition>();
+
+            effects = new List<PdSimEffect>();
+
+        }
+    }
+
+
+    /// <summary>
+    /// Instantaneous action.
+    /// e.g. Classical planning actions.
+    /// </summary>
+    [Serializable]
+    public class PdSimInstantaneousAction : PdSimAction
+    {
+        public PdSimInstantaneousAction(Action action) : base(action)
+        {
+        }
+    }
+
+    [Serializable]
+    public class PdSimDurativeAction : PdSimAction
+    {
+        public PdSimDuration duration;
+        public PdSimDurativeAction(Action action) : base(action)
+        {
+            duration = new PdSimDuration(action.Duration);
+        }
+    }
+
+
+    [Serializable]
+    public class PdSimCondition
+    {
+        public PdSimCondition(Condition condition)
+        {
+        }
+    }
+
+    [Serializable]
+    public class PdSimEffect
+    {
+
+        public PdSimEffect(Effect effect)
+        {
+
+        }
+    }
+    /// <summary>
+    /// Class that represents an action instance in the problem.
+    /// e.g. (move truck1 location1 location2) as in a plan.
+    /// </summary>
+    [Serializable]
+    public class PdSimActionInstance
+    {
+        /// <summary>
+        /// Id of the action instance.
+        /// </summary> 
+        public string id;
+        /// <summary>
+        /// Action name.
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// List of objects that are parameters of the action.
+        /// </summary>
+        public List<string> parameters;
+        public float startTime;
+        public float endTime;
+
+        public PdSimActionInstance(ActionInstance actionInstance)
+        {
+            id = actionInstance.Id;
+
+            name = actionInstance.ActionName;
+
+            parameters = new List<string>();
+            foreach (var parameter in actionInstance.Parameters)
+            {
+                parameters.Add(parameter.Symbol);
+            }
+
+            startTime = -1f;
+            endTime = -1f;
+
+            if (actionInstance.StartTime != null)
+            {
+                startTime = PdSimUtils.RealToFloat(actionInstance.StartTime);
+                endTime = PdSimUtils.RealToFloat(actionInstance.EndTime);
+            }
+        }
+    }
+
+    [Serializable]
+    public class PdSimDuration
+    {
+        public float upperBound;
+        public float lowerBound;
+
+        public PdSimDuration(Duration duration)
+        {
+            var atomUpperBound = duration.ControllableInBounds.Upper.Atom;
+            var atomLowerBound = duration.ControllableInBounds.Lower.Atom;
+
+            // check if atom is int or real
+            if (atomUpperBound.ContentCase == Atom.ContentOneofCase.Int)
+            {
+                upperBound = atomUpperBound.Int;
+            }
+
+            if (atomLowerBound.ContentCase == Atom.ContentOneofCase.Int)
+            {
+                lowerBound = atomLowerBound.Int;
+            }
+
+            if (atomUpperBound.ContentCase == Atom.ContentOneofCase.Real)
+            {
+                upperBound = PdSimUtils.RealToFloat(atomUpperBound.Real);
+            }
+
+            if (atomLowerBound.ContentCase == Atom.ContentOneofCase.Real)
+            {
+                lowerBound = PdSimUtils.RealToFloat(atomLowerBound.Real);
+            }
+        }
+
+    }
 
 }
