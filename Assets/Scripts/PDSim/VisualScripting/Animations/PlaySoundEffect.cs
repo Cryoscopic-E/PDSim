d1b2c2cd2c85570a99efa5e67cde13d12d5d751a
Emanuele <emanueledepellegrin@live.it>
2023-11-04 23:03:26 +0000

    sprite and sound

--- Assets/Scripts/PDSim/VisualScripting/Animations/PlaySoundEffect.cs
+++ Assets/Scripts/PDSim/VisualScripting/Animations/PlaySoundEffect.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlaySoundEffect : MonoBehaviour
+namespace PDSim.Animations
 {
-    // Start is called before the first frame update
-    void Start()
+    [UnitCategory("PDSim/Animations")]
+    public class PlaySoundEffect : Unit
     {
-        
-    }
+        [DoNotSerialize]
+        public ControlInput inputTrigger;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [DoNotSerialize]
+        public ControlOutput outputTrigger;
+
+        [DoNotSerialize]
+        public ValueInput audioSource;
+
+        [DoNotSerialize]
+        public ValueInput audioClip;
+
+        [DoNotSerialize]
+        public ValueInput volume;
+
+        [DoNotSerialize]
+        public ValueInput pitch;
+
+
+
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput("inputTrigger", (flow) => { return PlayAudio(flow); });
+            outputTrigger = ControlOutput("outputTrigger");
+
+            audioSource = ValueInput<AudioSource>("audioSource", null);
+            audioClip = ValueInput<AudioClip>("audioClip", null);
+
+            volume = ValueInput<float>("volume", 1f);
+            pitch = ValueInput<float>("pitch", .1f);
+
+            Succession(inputTrigger, outputTrigger);
+
+        }
+
+        private ControlOutput PlayAudio(Flow flow)
+        {
+            var source = flow.GetValue<AudioSource>(this.audioSource);
+            var clip = flow.GetValue<AudioClip>(this.audioClip);
+
+            source.clip = clip;
+            source.volume = flow.GetValue<float>(this.volume);
+            source.pitch = flow.GetValue<float>(this.pitch);
+            source.Play();
+
+            return outputTrigger;
+        }
     }
 }
